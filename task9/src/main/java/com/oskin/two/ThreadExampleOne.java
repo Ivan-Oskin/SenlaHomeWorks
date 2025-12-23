de60b131150caec2f2be5e018086b28211b4240a
Ivan-Oskin <Serivanoskin123@gmail.com>
2025-12-23 21:46:27 +0300

    Переделал второе задание

--- task9/src/main/java/com/oskin/two/ThreadExampleOne.java
+++ task9/src/main/java/com/oskin/two/ThreadExampleOne.java
@@ -1,15 +1,26 @@
 package com.oskin.two;
 
 public class ThreadExampleOne extends Thread{
+    private volatile boolean flag = true;
+    private Byte i = 1;
     @Override
-    public void run(){
-        ThreadExampleTwo threadExampleTwo = new ThreadExampleTwo();
-        threadExampleTwo.start();
-        try{
-            threadExampleTwo.join();
-        } catch (InterruptedException e){
-
+    public void run() {
+        while (flag){
+            synchronized (i){
+                while (TwoThreads.getRunOneThread()){
+                    try{
+                        i.wait();
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Моя имя: "+this.getName());
+                TwoThreads.setRunOneThread(true);
+                i.notifyAll();
+            }
         }
-        System.out.println("Моя имя: "+this.getName());
+    }
+    public void setFlag(boolean flag){
+        this.flag = flag;
     }
 }
--- task9/src/main/java/com/oskin/two/ThreadExampleTwo.java
+++ task9/src/main/java/com/oskin/two/ThreadExampleTwo.java
@@ -1,8 +1,26 @@
 package com.oskin.two;
 
 public class ThreadExampleTwo extends Thread{
+    private volatile boolean flag = true;
+    private Byte i = 1;
     @Override
-    public void run(){
-        System.out.println("Моя имя: "+this.getName());
+    public void run() {
+        while (flag){
+            synchronized (i){
+                while (!TwoThreads.getRunOneThread()){
+                    try{
+                        i.wait();
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Моя имя: "+this.getName());
+                TwoThreads.setRunOneThread(false);
+                i.notifyAll();
+            }
+        }
+    }
+    public void setFlag(boolean flag){
+        this.flag = flag;
     }
 }
