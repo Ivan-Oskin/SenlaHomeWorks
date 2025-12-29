15c751929841e58b29e0ed1a39a65d6278283ae4
Ivan-Oskin <Serivanoskin123@gmail.com>
2025-12-29 12:57:49 +0300

    Создал таблицы, заполнил первыми данными

---
+++ task10/CreateProduct.sql
@@ -0,0 +1,5 @@
+CREATE TABLE product (
+	maker varchar(10) NOT NULL,
+	model varchar(50) NOT NULL PRIMARY KEY,
+	type varchar(50) NOT NULL
+)
---
+++ task10/InsertProduct.sql
@@ -0,0 +1,10 @@
+INSERT INTO product (maker, model, type) VALUES
+('A', 'PC-1001', 'PC'),
+('A', 'Laptop-2001', 'Laptop'),
+('A', 'Printer-3001', 'Printer'),
+('B', 'PC-1002', 'PC'),
+('B', 'Laptop-2002', 'Laptop'),
+('C', 'Printer-3003', 'Printer'),
+('D', 'PC-1004', 'PC'),
+('E', 'Laptop-2005', 'Laptop'),
+('E', 'Printer-3005', 'Printer');
