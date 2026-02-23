d439bd809e8e349447f2df23e270652f10ef657f
Ivan-Oskin <Serivanoskin123@gmail.com>
2026-02-23 10:47:26 +0300

    .

---
+++ autoservice-app/autoservice/src/main/java/com/oskin/autoservice/ApplicationInitialize.java
@@ -0,0 +1,10 @@
+package com.oskin.autoservice;
+
+import liquibase.integration.servlet.GenericServletWrapper;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ServletContextAware;
+
+public class ApplicationInitialize implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext)
+}
---
+++ autoservice-app/autoservice/src/main/java/com/oskin/autoservice/controller/PlaceController.java
@@ -0,0 +1,5 @@
+package com.oskin.autoservice.controller;
+
+public class PlaceController {
+
+}
